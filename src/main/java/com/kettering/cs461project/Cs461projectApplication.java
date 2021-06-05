fba27aff52249eac012b8320418e3ec1ecbe9bf2
Kyle Heestand <kheestand@heestand.tech>
2021-06-05 19:52:32 -0400

    Added Spring Framework

---
+++ src/main/java/com/kettering/cs461project/Cs461projectApplication.java
@@ -0,0 +1,13 @@
+package com.kettering.cs461project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs461projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs461projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kettering/cs461project/Cs461projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kettering.cs461project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs461projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
